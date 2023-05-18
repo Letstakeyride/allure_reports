338154b4519a77fdbcf1b37d0224331421b23c53
Mochinut <waka332@Mail.ru>
2023-05-18 19:39:02 +0300

    Initial commit

---
+++ tests/test_only_selene.py
@@ -0,0 +1,17 @@
+from selene.support import by
+from selene.support.conditions import be
+from selene import browser
+
+
+
+def test_github():
+    browser.open_url("https://github.com/")
+
+    browser.element(".header-search-input").click()
+    browser.element(".header-search-input").send_keys("Letstakeyride/test_for_qa_guru")
+    browser.element(".header-search-input").submit()
+
+    browser.element(by.link_text("Letstakeyride/test_for_qa_guru")).click()
+    browser.element("#issues-tab").click()
+
+    browser.element(by.partial_text("#1")).should(be.visible)
