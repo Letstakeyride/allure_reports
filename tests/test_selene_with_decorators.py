6890a12dfd07421d5c84b4cf06e310886235362d
Mochinut <waka332@Mail.ru>
2023-05-18 20:14:11 +0300

    .

---
+++ tests/test_selene_with_decorators.py
@@ -0,0 +1,34 @@
+import allure
+from selene.support import by
+from selene.support.conditions import be
+from selene import browser
+
+
+def test_decorator_steps():
+    open_main_page()
+    search_for_repository("Letstakeyride/test_for_qa_guru")
+    open_issue_tab()
+    should_see_issue_with_number("#1")
+
+
+@allure.step("Открываем главную страницу")
+def open_main_page():
+    browser.open_url("https://github.com")
+
+
+@allure.step("Ищем репозитория {repo}")
+def search_for_repository(repo):
+    browser.element(".header-search-input").click()
+    browser.element(".header-search-input").send_keys("Letstakeyride/test_for_qa_guru")
+    browser.element(".header-search-input").submit()
+
+
+@allure.step("Открываем таб Issues")
+def open_issue_tab():
+    browser.element(by.link_text("Letstakeyride/test_for_qa_guru")).click()
+    browser.element("#issues-tab").click()
+
+
+@allure.step("Проверяем наличие Issue с номером {number}")
+def should_see_issue_with_number(number):
+    browser.element(by.partial_text(number)).should(be.visible)
--- tests/test_selene_with_labels.py
+++ tests/test_selene_with_labels.py
@@ -1,9 +1,15 @@
 import allure
+from allure_commons.types import Severity
 from selene.support import by
 from selene.support.conditions import be
 from selene import browser
 
 
+@allure.tag("web")
+@allure.label("owner", "agargalyk")
+@allure.feature("Поиск в git")
+@allure.story("Ищем issue в репозитории git")
+@allure.link("https://github.com", name="Testing")
 def test_github():
     with allure.step('Открываем гит'):
         browser.open_url("https://github.com/")
@@ -15,4 +21,4 @@ def test_github():
         browser.element(by.link_text("Letstakeyride/test_for_qa_guru")).click()
         browser.element("#issues-tab").click()
     with allure.step('Проверяем наличие issue'):
-     browser.element(by.partial_text("#1")).should(be.visible)
+        browser.element(by.partial_text("#1")).should(be.visible)
