f19f07484ef0f7a82c037b8031b531305a6c3c9c
Mochinut <waka332@Mail.ru>
2023-05-23 18:33:03 +0300

    .

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,11 @@
 import os
-
+import attach
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
 from dotenv import load_dotenv
 
-from utils import attach
 
 DEFAULT_BROWSER_VERSION = "100.0"
 
